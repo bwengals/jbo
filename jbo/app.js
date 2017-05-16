0e1200cbd9fc746edfd5d7b89eb9653a90d8f56c
Bill Engels <w.j.engels@gmail.com>
2017-05-16 15:42:27 -0700

    modified version of Node-Chat

--- jbo/app.js
+++ jbo/app.js
@@ -1,46 +1,102 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var router = express.Router();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+var uuid = require('uuid');
+app.use(express.static('public'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var clients = [];
+io.sockets.on('connect', function(client) {
+    clients.push(client);
+    console.log('New User' + client.id);
+    for (var i = clients.length - 1; i >= 0; i--) {
+        if (typeof clients[i] !== "undefined") {
+            clients[i].emit('users-online', clients.length);
+        }
+    }
 
-app.use('/', index);
-app.use('/users', users);
+    client.on('user-ready-blue', function() {
+        clients[clients.indexOf(client)].ready = true;
+        clients[clients.indexOf(client)].hasPair = false;
+        clients[clients.indexOf(client)].color = 'blue';
+    });
+    
+	client.on('user-ready-red', function() {
+        clients[clients.indexOf(client)].ready = true;
+        clients[clients.indexOf(client)].hasPair = false;
+        clients[clients.indexOf(client)].color = 'red';
+    });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    client.on('not-ready', function() {
+        clients[clients.indexOf(client)].ready = false;
+        clients[clients.indexOf(client)].hasPair = true;
+    });
+
+    client.on('disconnect', function() {
+        clients.splice(clients.indexOf(client), 1);
+        console.log('disconnect: ' + client.id);
+        console.log('\n ----  Current Clients ---');
+        for (var i = clients.length - 1; i >= 0; i--) {
+            if (typeof clients[i] !== "undefined") {
+                clients[i].emit('users-online', clients.length);
+                console.log('Client ' + i + ' :' + clients[i].id);
+            } else {
+                console.log('Undef');
+            }
+        }
+        console.log('---- End --- \n');
+    });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//poll to match users
+function pollFunc(fn, interval) {
+    interval = interval || 1000;
+    (function p() {
+        fn();
+        setTimeout(p, interval);
+    })();
+}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+pollFunc(function() {
+    var pairs = [];
+    for (var i = clients.length - 1; i >= 0; i--) {
+        var currUser;
+		// check if user is ready and not in a conversation already
+        if (clients[i].ready && !clients[i].hasPair) {
+            currUser = clients[i];
+            for (var c = clients.length - 1; c >= 0; c--) {
+                if (clients[c].ready && !clients[c].hasPair && currUser !== clients[c] && currUser.color !== clients[c].color) {
+                    pairs.push({
+                        a: currUser,
+                        b: clients[c]
+                    });
+                    currUser.hasPair = true;
+                    clients[c].hasPair = true;
+                }
+            }
+        }
+    }
+    //
+    for (var b = pairs.length - 1; b >= 0; b--) {
+        var id = uuid.v4();
+        console.log('Pair Made Between: "' + pairs[b].a.id + '"" and "' + pairs[b].b.id + '"" to join room ' + id);
+        pairs[b].a.emit('id', id);
+        pairs[b].b.emit('id', id);
+    }
+}, 1000);
+
+
+router.get('/', function(req, res) {
+    res.sendfile(__dirname + '/views/home.html');
 });
 
-module.exports = app;
+router.get('/chat/:id', function(req, res) {
+    res.sendfile(__dirname + '/views/chat.html');
+});
+
+app.set('port', process.env.PORT || 1337);
+console.log('listening on 1337')
+app.use('/', router);
+server.listen(app.get('port'));
